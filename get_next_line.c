1ea502e0f3243bbb23a2a159afa33c236a411834
Naomi Hariman <nhariman@f1r5s3.codam.nl>
2020-01-09 19:51:16 +0100

    rewriting gnl because spaghetti is bad

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nhariman <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/04 20:04:50 by nhariman       #+#    #+#                */
-/*   Updated: 2020/01/08 22:01:59 by nhariman      ########   odam.nl         */
+/*   Updated: 2020/01/09 19:50:30 by nhariman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,40 @@ int			free_buffers(char *buffer, char *restbuf, char **line)
 	return (-1);
 }
 
+
+int			get_next_line(int fd, char **line)
+{
+	int			bytes_read;
+	char		buf[BUFFER_SIZE + 1];
+	static char	*restbuf;
+
+	bytes_read = 1;
+	buf[BUFFER_SIZE] = '\0';
+	if (!line || fd < 0)
+		return (-1);
+	*line = ft_strdup("");
+	if (restbuf)
+		check_restbuf(line ,restbuf); 
+	/* things check_restbuf has to handle:
+	needs to check if there's a newline somewhere in the restbuf
+	- if so, return the line until said newline, do not read more!!
+	- if it finds a newline at the start: line is empty string, restbuf updates to skip one past the printed newline
+	- if the restbuf does not contain a newline, put it in line and frees the buffer 
+	- boolean return? if 1: return line but there's still something in restbuf do not read, 0: restbuf is empty a new read can be done
+	*/
+	while (bytes_read > 0)
+	{
+		/* create a function that reads using BUFFER_SIZE
+		- find the newline, if a newline is found, at the start, line = empty string, restbuf = the rest of the content in buffer
+		  !!important! this function should only be called if there are no newlines in the static restbuf!
+		- if buf has no newline, put entire content in line
+		- if only newline, return empty string.
+		*/
+	}
+}
+
 /* end of file can be found by checking if less than */
-/* the BUFFER_SIZE has been read */
+/* the BUFFER_SIZE has been read 
 int			get_next_line(int fd, char **line)
 {
 	int			bytes_read;
@@ -75,7 +107,8 @@ int			get_next_line(int fd, char **line)
 		}
 	}
 	return (1);
-}
+} */
+
 
 int		main(void)
 {
